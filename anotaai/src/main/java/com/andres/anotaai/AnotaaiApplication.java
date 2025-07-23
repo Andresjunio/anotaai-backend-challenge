2f27a366585f56a2846e12bd067679125195743f
Andres Junio <andresestudos@gmail.com>
2025-07-23 05:42:05 -0300

    Initial commit

---
+++ anotaai/src/main/java/com/andres/anotaai/AnotaaiApplication.java
@@ -0,0 +1,13 @@
+package com.andres.anotaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotaaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotaaiApplication.class, args);
+	}
+
+}
